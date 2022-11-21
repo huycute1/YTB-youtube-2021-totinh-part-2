603a83b4932c245ae834c21523f50aa6e9e88e5f
huycute1 <118709262+huycute1@users.noreply.github.com>
2022-11-21 13:42:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
-    reasonPlaceholder: 'Lý do',
-    btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    introTitle: 'Hello chị yêu',
+    introDesc: `Em có điều muốn nói với chị nè`,
+    btnIntro: 'Oki',
+    title: 'Chị có yêu em hoonggg 😚😚😚',
+    desc: 'Nếu chị có yêu nói đi ngại gì :)))',
+    btnYes: 'Có',
+    btnNo: 'Không',
+    question:'Hehe em biết mà, yêu chị nhất trên đời',
+    reasonPlaceholder: 'Chị có điều gì muốn nói với em không ',
+    btnReply: 'Gửi em',
+    reply: 'I LOVE YOU ❤️❤️❤️',
+    mess: 'I LOVE YOU TOO ❤️',
+    messDesc: '💬💬 Nhắn tin cho em nha. 💬💬',
+    btnAccept: 'Oki',
+    messLink: 'https://m.me/huyxinhgai.net'
 }
